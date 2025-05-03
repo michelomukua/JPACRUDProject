b03da209e72176d4ba7ec3a6c87b4a8477d5e072
Michel <michelmukua@Ryan-s-S20.localdomain>
2025-05-02 18:18:08 -0600

    create spring boot mvc project

---
+++ FitnessClubs/src/main/java/com/skilldistillery/fitnessclubs/FitnessClubsApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.fitnessclubs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitnessClubsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitnessClubsApplication.class, args);
+	}
+
+}
---
+++ FitnessClubs/src/test/java/com/skilldistillery/fitnessclubs/FitnessClubsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.fitnessclubs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnessClubsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
